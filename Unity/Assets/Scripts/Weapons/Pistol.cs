90451cfb3adc0a480fbdfec38a4f85f6772f5077
aokurr <41aokur41@gmail.com>
2022-01-29 00:12:27 +0300

    YÜKLEME

---
+++ Unity/Assets/Scripts/Weapons/Pistol.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pistol : Weapon
+{
+    public Pistol(D_Pistol data, int ammoLeft) : base(ammoLeft)
+    {
+        fireRate = data.fireRate;
+        movementSpeed = data.movementSpeed;
+        damageAmount = data.damageAmount;
+        maxAmmo = data.maxAmmo;
+        ammoLeft = this.ammoLeft;
+        id = 1;
+    }
+}
